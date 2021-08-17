d244ed00d49c2ce6927a1779244aabc679f16046
Peter <prigali>
2021-08-17 04:31:15 -0500

    remove old lists

--- Call-Of-Duty-Cold-War-Zombies/Utils/health_armour.py
+++ Call-Of-Duty-Cold-War-Zombies/Utils/health_armour.py
@@ -49,13 +49,8 @@ class Health:
                     out_health_lst.append(health_lst[-1])
 
             self.hp = out_health_lst[self.level]
-            # self.hp = [150, 510, 950, 2150, 5750, 13500, 23450, 30000][self.level - 1]
         else:
             self.hp = health_lst[self.level]
-            # self.hp = [150, 210, 270, 330, 390, 450, 510, 570, 630, 690, 750, 850, 950, 1050,
-            #            1150, 1250, 1550, 1850, 2150, 2450, 2750, 3500, 4250, 5000, 5750,
-            #            6500, 7900, 9300, 10700, 12100, 13500, 15250, 17000, 18750, 20500,
-            #            22250, 23450, 24650, 25850, 27050, 28250, 29450, 30000][self.level - 1]
 
     def get_health(self) -> float:
         return float(self.hp)
