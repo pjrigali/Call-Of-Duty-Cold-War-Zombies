3e446e28680ac40931be44efd94f5780d077d70a
Peter <prigali>
2021-08-16 20:20:30 -0500

    zombie health now accounts for health cap

--- Call-Of-Duty-Cold-War-Zombies/Utils/health_armour.py
+++ Call-Of-Duty-Cold-War-Zombies/Utils/health_armour.py
@@ -10,63 +10,55 @@ class Health:
 
     def __init__(self,
                  level: int,
+                 health_cap: int = 55,
                  outbreak: bool = False):
 
         self.level = level
+        self.health_cap = health_cap
         self.outbreak = outbreak
 
-        if outbreak:
-            if level >= 7:
-                self.level = 7
-            else:
-                pass
-        else:
-            if level >= 43:
-                self.level = 43
-            else:
-                pass
+        health_lst = []
+        health_amount = 150
+        for num in range(self.health_cap + 1):
+            if 1 < num <= 11:
+                health_amount += 60
+            elif 11 < num <= 16:
+                health_amount += 100
+            elif 16 < num <= 21:
+                health_amount += 300
+            elif 21 < num <= 26:
+                health_amount += 750
+            elif 26 < num <= 31:
+                health_amount += 1400
+            elif 31 < num <= 36:
+                health_amount += 1750
+            elif 36 < num <= 85:
+                health_amount += 2000
+            elif 85 < num:
+                health_amount = 120000
+            health_lst.append(health_amount)
 
         if outbreak:
-            self.hp = [150, 510, 950, 2150, 5750, 13500, 23450, 30000][self.level - 1]
+            out_health_lst = []
+            for num in range(self.level):
+                if num <= 1:
+                    out_health_lst.append(150)
+                elif 1 < num < 10:
+                    out_health_lst.append(health_lst[1 + (num * 6)])
+                else:
+                    out_health_lst.append(health_lst[-1])
+
+            self.hp = out_health_lst[self.level]
+            # self.hp = [150, 510, 950, 2150, 5750, 13500, 23450, 30000][self.level - 1]
         else:
-            self.hp = [150, 210, 270, 330, 390, 450, 510, 570, 630, 690, 750, 850, 950, 1050,
-                       1150, 1250, 1550, 1850, 2150, 2450, 2750, 3500, 4250, 5000, 5750,
-                       6500, 7900, 9300, 10700, 12100, 13500, 15250, 17000, 18750, 20500,
-                       22250, 23450, 24650, 25850, 27050, 28250, 29450, 30000][self.level - 1]
+            self.hp = health_lst[self.level]
+            # self.hp = [150, 210, 270, 330, 390, 450, 510, 570, 630, 690, 750, 850, 950, 1050,
+            #            1150, 1250, 1550, 1850, 2150, 2450, 2750, 3500, 4250, 5000, 5750,
+            #            6500, 7900, 9300, 10700, 12100, 13500, 15250, 17000, 18750, 20500,
+            #            22250, 23450, 24650, 25850, 27050, 28250, 29450, 30000][self.level - 1]
 
     def get_health(self) -> float:
-
         return float(self.hp)
 
-
-class Armour:
-
-    def __init__(self,
-                 level: int,
-                 outbreak: bool = False):
-
-        self.level = level
-        self.outbreak = outbreak
-
-        if outbreak:
-            if level >= 7:
-                self.level = 7
-            else:
-                pass
-        else:
-            if level >= 43:
-                self.level = 43
-            else:
-                pass
-
-        if outbreak:
-            self.arm = [150, 510, 950, 2150, 5750, 13500, 23450, 30000][self.level - 1]
-        else:
-            self.arm = [150, 210, 270, 330, 390, 450, 510, 570, 630, 690, 750, 850, 950, 1050,
-                        1150, 1250, 1550, 1850, 2150, 2450, 2750, 3500, 4250, 5000, 5750,
-                        6500, 7900, 9300, 10700, 12100, 13500, 15250, 17000, 18750, 20500,
-                        22250, 23450, 24650, 25850, 27050, 28250, 29450, 30000][self.level - 1]
-
-    def get_armour(self) -> float:
-
-        return float(self.arm / 2)
+    def get_armour(self, multiplier: int = 2) -> float:
+        return float(self.hp / multiplier)
