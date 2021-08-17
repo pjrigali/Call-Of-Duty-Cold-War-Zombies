20c1048c15640515ea836bf14d0c97431a5fa851
Peter <prigali>
2021-08-17 04:56:54 -0500

    asjustments to analysis to become a child class

--- Call-Of-Duty-Cold-War-Zombies/Utils/analysis.py
+++ Call-Of-Duty-Cold-War-Zombies/Utils/analysis.py
@@ -5,39 +5,49 @@ Created on Thu Apr 22 21:08:22 2021
 @author: Peter
 """
 import numpy as np
-from numpy.random import normal
 import pandas as pd
 from typing import List
 import matplotlib.pyplot as plt
-from random import choices, seed
-seed(1)
+import random
+from Utils.processor import Build
+random.seed(1)
 
 
-class Analyze:
+class Analyze(Build):
 
-    def __init__(self, build, max_range: int = 200):
+    def __init__(self, weapon_class_levels: dict = None, perk_class_levels: dict = None, max_range: int = 200):
 
-        self.consecutive = build.consecutive
-        self.stats = build.stats
-        self.col_lst = build.col_lst
+        self._build = Build(weapon_class_levels=weapon_class_levels, perk_class_levels=perk_class_levels)
         self.max_range = max_range
-        self.gun_names = list(build.stats.keys())
 
-        def shot_distribution() -> np.ndarray:
-            acc = [i.gun_acc_value for i in build.stats.values()]
-            acc_n = [i for i in acc if i != 0]
-            sig1 = np.around(np.std(acc_n), 3)
-            mu1 = np.around(np.mean(acc_n), 3)
-            return np.around(normal(mu1, sig1, 2500), 3)
+    def process(self, weapon_dic: dict):
+        new_weapon_dic = {}
+        for i in ['weapon', 'nickname', 'equipped_attachments', 'rarity', 'pap', 'accuracy', 'critical']:
+            if i in weapon_dic.keys():
+                new_weapon_dic[i] = weapon_dic[i]
+            else:
+                new_weapon_dic[i] = None
 
-        self.hits = shot_distribution()
+        return self._build._process(weapon=new_weapon_dic['weapon'],
+                                    nickname=new_weapon_dic['nickname'],
+                                    equipped_attachments=new_weapon_dic['equipped_attachments'],
+                                    rarity=new_weapon_dic['rarity'],
+                                    pap=new_weapon_dic['pap'],
+                                    accuracy=new_weapon_dic['accuracy'],
+                                    critical=new_weapon_dic['critical'])
+
+    def process_multi(self, weapon_dic_lst: List[dict]) -> list:
+        return [self.process(weapon_dic=i) for i in weapon_dic_lst]
+
+    def get_attachments(self, weapon_dic: dict, equipped_dic: dict = None):
+        return self._build._get_attachments(weapon_dic=weapon_dic, equipped_dic=equipped_dic)
 
     def mu_shots(self, weapon_dic: dict) -> np.ndarray:
         ac = weapon_dic['Accuracy']
-        dis = self.hits
+        dis = self._build.hits
         ind = np.cumsum([1 if i <= ac else 0 for i in dis])
 
-        if self.consecutive:
+        if self._build.consecutive:
             con = [0, 1.00, 1.02, 1.04, 1.06, 1.08, 1.10, 1.12, 1.14, 1.16, 1.18, 1.20] + ([1.20] * 40)
         else:
             con = [1.00] * 50
@@ -56,14 +66,13 @@ class Analyze:
         con_lst_master_n = sum(con_lst_master, [])
         con_shots = [con[i] for i in con_lst_master_n]
         crit1 = weapon_dic['Critical']
-        headshots = choices([0, 1], k=len(con_shots), weights=[1 - crit1, crit1])
+        headshots = random.choices([0, 1], k=len(con_shots), weights=[1 - crit1, crit1])
         headshot_multi = weapon_dic['Crit']
         temp_lst = list(
             np.around([con_shots[i] * headshot_multi if j == 1.00 else 1.00 for i, j in enumerate(headshots)], 2))
         arr = np.array(temp_lst + ([0] * (2500 - len(temp_lst))))
-
-        mc = int(weapon_dic['Mag Capacity'])
-        average_shots = np.ceil(np.mean([sum([arr[k - q] for q in range(mc)]) for k, l in enumerate(arr)]))
+        mc = range(int(weapon_dic['Mag Capacity']))
+        average_shots = np.ceil(np.mean([sum([arr[k - q] for q in mc]) for k, l in enumerate(arr)]))
 
         return average_shots
 
@@ -132,35 +141,33 @@ class Analyze:
         return np.around([damage_range_arr[i] * shots_average for i, j in enumerate(damage_range_arr)], 0)
 
     def movement_ratio(self, weapon_dic: dict) -> float:
-        lst = {
-            'Movement Speed': [0, 0, 0],
-            'Sprinting Speed': [0, 0, 0],
-            'Shooting Speed': [0, 0, 0],
-            'Sprint to Fire': [0, 0, 0],
-            'Aim Walking': [0, 0, 0],
-            'ADS': [0, 0, 0]
-        }
+        lst = {'Movement Speed': [0, 0, 0],
+               'Sprinting Speed': [0, 0, 0],
+               'Shooting Speed': [0, 0, 0],
+               'Sprint to Fire': [0, 0, 0],
+               'Aim Walking': [0, 0, 0],
+               'ADS': [0, 0, 0]}
 
         for i in lst.keys():
             temp_lst = []
-            for j in self.gun_names:
+            for j in self._build.gun_names:
                 if i == 'Movement Speed':
-                    temp_lst.append(self.stats[j].movement)
+                    temp_lst.append(self._build.stats[j].movement)
                     continue
                 elif i == 'Sprinting Speed':
-                    temp_lst.append(self.stats[j].sprint)
+                    temp_lst.append(self._build.stats[j].sprint)
                     continue
                 elif i == 'Shooting Speed':
-                    temp_lst.append(self.stats[j].shoot_speed)
+                    temp_lst.append(self._build.stats[j].shoot_speed)
                     continue
                 elif i == 'Sprint to Fire':
-                    temp_lst.append(self.stats[j].sprint_to_fire)
+                    temp_lst.append(self._build.stats[j].sprint_to_fire)
                     continue
                 elif i == 'Aim Walking':
-                    temp_lst.append(self.stats[j].aim_walking)
+                    temp_lst.append(self._build.stats[j].aim_walking)
                     continue
                 elif i == 'ADS':
-                    temp_lst.append(self.stats[j].ads)
+                    temp_lst.append(self._build.stats[j].ads)
                     continue
 
             lst[i] = [min(temp_lst), max(temp_lst), weapon_dic[i]]
@@ -170,27 +177,25 @@ class Analyze:
         return ratio
 
     def control_ratio(self, weapon_dic: dict) -> float:
-        lst = {
-            'Vertical Recoil': [0, 0, 0],
-            'Horizontal Recoil': [0, 0, 0],
-            'Centering Speed': [0, 0, 0],
-            'Hip Fire': [0, 0, 0],
-        }
+        lst = {'Vertical Recoil': [0, 0, 0],
+               'Horizontal Recoil': [0, 0, 0],
+               'Centering Speed': [0, 0, 0],
+               'Hip Fire': [0, 0, 0]}
 
         for i in lst.keys():
             temp_lst = []
-            for j in self.gun_names:
+            for j in self._build.gun_names:
                 if i == 'Vertical Recoil':
-                    temp_lst.append(self.stats[j].vertical_recoil)
+                    temp_lst.append(self._build.stats[j].vertical_recoil)
                     continue
                 if i == 'Horizontal Recoil':
-                    temp_lst.append(self.stats[j].horizontal_recoil)
+                    temp_lst.append(self._build.stats[j].horizontal_recoil)
                     continue
                 if i == 'Centering Speed':
-                    temp_lst.append(self.stats[j].centering_speed)
+                    temp_lst.append(self._build.stats[j].centering_speed)
                     continue
                 if i == 'Hip Fire':
-                    temp_lst.append(self.stats[j].hip_fire)
+                    temp_lst.append(self._build.stats[j].hip_fire)
                     continue
             lst[i] = [min(temp_lst), max(temp_lst), weapon_dic[i]]
 
@@ -208,7 +213,6 @@ class Analyze:
         long_shot = weapon_dic['Long Shot']
         t = list(damage_range_arr)
         temp_lst = []
-
         for k, j in enumerate(t):
             if k <= 4:
                 temp_lst.append(j * close)
@@ -219,98 +223,41 @@ class Analyze:
 
         return np.array(temp_lst)
 
-    def compare(self, weapon_dic: dict, zombie_health: float, for_viz: bool = False, armour: bool = False):
+    def compare(self, weapon_dic: dict, zombie_health: float, zombie_armour: float = None, for_viz: bool = False):
         mu = self.mu_shots(weapon_dic)
         damage_range = self.check_multi(damage_range_arr=self.damage_range(weapon_dic=weapon_dic), weapon_dic=weapon_dic)
+        if zombie_armour:
+            zombie_health = zombie_health + zombie_armour
+            damage_range = np.multiply(np.divide(damage_range, 4), weapon_dic['Armour Damage'])
 
-        if armour:
-            armour_damage_range = np.multiply(np.divide(damage_range, 4), weapon_dic['Armour Damage'])
-            if for_viz:
-                temp_dic = {
-                    'Damage Per Clip': self.damage_per_clip(damage_range_arr=armour_damage_range,
-                                                            shots_average=mu),
-                    'Damage Per Second': self.damage_per_second(weapon_dic=weapon_dic,
-                                                                damage_range_arr=armour_damage_range),
-                    'Time To Kill': self.time_to_kill(weapon_dic=weapon_dic,
-                                                      damage_range_arr=damage_range,
-                                                      zombie_health=zombie_health) +
-                                    self.time_to_kill(weapon_dic=weapon_dic,
-                                                      damage_range_arr=armour_damage_range,
-                                                      zombie_health=zombie_health + armour),
-                    'Shots To Kill': self.shots_to_kill(damage_range_arr=damage_range,
-                                                        zombie_health=zombie_health) +
-                                     self.shots_to_kill(damage_range_arr=armour_damage_range,
-                                                        zombie_health=zombie_health + armour),
-                    'Damage Per Max Ammo': self.damage_per_max_ammo(weapon_dic=weapon_dic,
-                                                                    damage_range_arr=armour_damage_range)
-                }
-
-                return [weapon_dic['Temp Name'], pd.DataFrame(temp_dic)]
-            else:
-                temp_dic = {'Temp Name': weapon_dic['Temp Name'],
-                            'Damage Per Clip': self.damage_per_clip(damage_range_arr=armour_damage_range,
-                                                                    shots_average=mu),
-                            'Damage Per Second': self.damage_per_second(weapon_dic=weapon_dic,
-                                                                        damage_range_arr=armour_damage_range),
-                            'Time To Kill': self.time_to_kill(weapon_dic=weapon_dic,
-                                                              damage_range_arr=damage_range,
-                                                              zombie_health=zombie_health) +
-                                            self.time_to_kill(weapon_dic=weapon_dic,
-                                                              damage_range_arr=armour_damage_range,
-                                                              zombie_health=zombie_health + armour),
-                            'Shots To Kill': self.shots_to_kill(damage_range_arr=damage_range,
-                                                                zombie_health=zombie_health) +
-                                             self.shots_to_kill(damage_range_arr=armour_damage_range,
-                                                                zombie_health=zombie_health + armour),
-                            'Damage Per Max Ammo': self.damage_per_max_ammo(weapon_dic=weapon_dic,
-                                                                            damage_range_arr=armour_damage_range),
-                            'Shooting Time': self.shoot_time(weapon_dic=weapon_dic),
-                            'Shoot To Reload Ratio': self.shoot_reload_ratio(weapon_dic=weapon_dic),
-                            'Movement Ratio': self.movement_ratio(weapon_dic=weapon_dic),
-                            'Control Ratio': self.control_ratio(weapon_dic=weapon_dic),
-                            'Drop Off Ratio': self.drop_off_ratio(weapon_dic=weapon_dic),
-                            }
-                return temp_dic
-        else:
-            if for_viz:
-                temp_dic = {
-                    'Damage Per Clip': self.damage_per_clip(damage_range_arr=damage_range,
-                                                            shots_average=mu),
-                    'Damage Per Second': self.damage_per_second(weapon_dic=weapon_dic,
-                                                                damage_range_arr=damage_range),
+        temp_dic = {'Damage Per Clip': self.damage_per_clip(damage_range_arr=damage_range, shots_average=mu),
+                    'Damage Per Second': self.damage_per_second(weapon_dic=weapon_dic, damage_range_arr=damage_range),
                     'Time To Kill': self.time_to_kill(weapon_dic=weapon_dic,
                                                       damage_range_arr=damage_range,
                                                       zombie_health=zombie_health),
-                    'Shots To Kill': self.shots_to_kill(damage_range_arr=damage_range,
-                                                        zombie_health=zombie_health),
+                    'Shots To Kill': self.shots_to_kill(damage_range_arr=damage_range, zombie_health=zombie_health),
                     'Damage Per Max Ammo': self.damage_per_max_ammo(weapon_dic=weapon_dic,
-                                                                    damage_range_arr=damage_range),
+                                                                    damage_range_arr=damage_range)
                     }
-                return pd.DataFrame(temp_dic)
-            else:
-                temp_dic = {'Temp Name': weapon_dic['Temp Name'],
-                            'Damage Per Clip': self.damage_per_clip(damage_range_arr=damage_range,
-                                                                    shots_average=mu),
-                            'Damage Per Second': self.damage_per_second(weapon_dic=weapon_dic,
-                                                                        damage_range_arr=damage_range),
-                            'Time To Kill': self.time_to_kill(weapon_dic=weapon_dic,
-                                                              damage_range_arr=damage_range,
-                                                              zombie_health=zombie_health),
-                            'Shots To Kill': self.shots_to_kill(damage_range_arr=damage_range,
-                                                                zombie_health=zombie_health),
-                            'Damage Per Max Ammo': self.damage_per_max_ammo(weapon_dic=weapon_dic,
-                                                                            damage_range_arr=damage_range),
-                            'Shooting Time': self.shoot_time(weapon_dic=weapon_dic),
-                            'Shoot To Reload Ratio': self.shoot_reload_ratio(weapon_dic=weapon_dic),
-                            'Movement Ratio': self.movement_ratio(weapon_dic=weapon_dic),
-                            'Control Ratio': self.control_ratio(weapon_dic=weapon_dic),
-                            'Drop Off Ratio': self.drop_off_ratio(weapon_dic=weapon_dic),
-                            }
 
-                return temp_dic
+        if for_viz:
+            return pd.DataFrame.from_dict(temp_dic)
+        else:
+            temp_dic['Temp Name'] = weapon_dic['Temp Name']
+            temp_dic['Shooting Time'] = self.shoot_time(weapon_dic=weapon_dic)
+            temp_dic['Shoot To Reload Ratio'] = self.shoot_reload_ratio(weapon_dic=weapon_dic)
+            temp_dic['Movement Ratio'] = self.movement_ratio(weapon_dic=weapon_dic)
+            temp_dic['Control Ratio'] = self.control_ratio(weapon_dic=weapon_dic)
+            temp_dic['Drop Off Ratio'] = self.drop_off_ratio(weapon_dic=weapon_dic)
+
+            return temp_dic
+
+    def compare_multi(self, weapon_dic_lst: List[dict], zombie_health: float, zombie_armour: float = None,
+                      for_viz: bool = True) -> dict:
+        return {i['Temp Name']: self.compare(weapon_dic=i, zombie_health=zombie_health, zombie_armour=zombie_armour,
+                                             for_viz=for_viz) for i in weapon_dic_lst}
 
     def build_df(self, weapon_dic_lst: List[dict], cols: List[str] = None) -> pd.DataFrame:
-
         data_n = []
         for dic in weapon_dic_lst:
             dic['Shooting Time'] = self.shoot_time(dic)
@@ -321,16 +268,16 @@ class Analyze:
             data_n.append(dic)
 
         if cols is None:
-            return pd.DataFrame(data_n)[self.col_lst].set_index('Temp Name')
+            return pd.DataFrame(data_n)[self._build.col_lst].set_index('Temp Name')
         else:
             return pd.DataFrame(data_n)[cols].set_index('Temp Name')
 
-    def viz(self, gun_data: dict, keyword: str, x_limit: int, ind: int, save_image: bool = False) -> None:
+    def viz(self, weapon_df_dic: dict, keyword: str, x_limit: int, ind: int, save_image: bool = False) -> None:
         n = 0
         df = pd.DataFrame()
-        for i in gun_data.keys():
-            df[i] = gun_data[i][keyword]
-            if keyword in gun_data[i].columns:
+        for i in weapon_df_dic.keys():
+            df[i] = weapon_df_dic[i][keyword]
+            if keyword in weapon_df_dic[i].columns:
                 n += 1
 
         cmap = [plt.get_cmap('viridis')(1. * i / n) for i in range(n)]
@@ -369,10 +316,5 @@ class Analyze:
         plt.show()
 
     def viz_all(self, weapon_df_dic: dict, x_limit: int, ind: int, save_image: bool) -> None:
-
         for keyword in ['Damage Per Max Ammo', 'Damage Per Clip', 'Damage Per Second', 'Time To Kill', 'Shots To Kill']:
-            self.viz(gun_data=weapon_df_dic,
-                     keyword=keyword,
-                     x_limit=x_limit,
-                     ind=ind,
-                     save_image=save_image)
+            self.viz(weapon_df_dic=weapon_df_dic, keyword=keyword, x_limit=x_limit, ind=ind, save_image=save_image)
