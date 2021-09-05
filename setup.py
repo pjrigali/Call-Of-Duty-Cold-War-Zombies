e08c656de777723e50b9ca752ca68ec4734ba680
Peter Rigali <25036405+pjrigali@users.noreply.github.com>
2021-09-05 04:17:24 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+current_path = os.path.abspath('../zombie')
+
+
+def read_file(*parts):
+    with open(os.path.join(current_path, *parts), encoding='utf-8') as reader:
+        return reader.read()
+
+
+setup(
+    name='cold_war_zombies',
+    version='3.0.3',
+    packages=find_packages(exclude=["venv", "dist", "plots", "docs"]),
+    # packages=['zombie',],
+    author='Peter Rigali',
+    author_email='peterjrigali@gmail.com',
+    license='MIT',
+    description='A package for comparing weapons in Call of Duty Cold War Zombies',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://medium.com/@peterjrigali/best-weapon-in-zombies-9fddd33735c5',
+)
