47a27823dd86373da584ac5104ac09446def0ae2
Peter <prigali>
2021-09-08 14:05:32 -0500

    update documentation and clean up file

--- zombie/health_armour.py
+++ zombie/health_armour.py
@@ -4,6 +4,7 @@ Created on Thu Apr 22 19:49:29 2021
 
 @author: Peter
 """
+from typing import Optional
 from dataclasses import dataclass
 
 
@@ -16,28 +17,31 @@ class Health:
 
     :param level: Desired zombie level to test weapon strength at.
     :type level: int
-    :param health_cap: The level when zombie health values stop increasing. In season 5 this is level 55. Season 4 was 85.
+    :param health_cap: The level when zombie health values stop increasing. In season 5 this is level 55.
+        Season 4 was 85. *Optional*
     :type health_cap: int
-    :param outbreak: If level is True, parameter is referencing an outbreak level.
-    :type outbreak: bool, default False
+    :param outbreak: If level is True, parameter is referencing an outbreak level, default is False. *Optional*
+    :type outbreak: bool
+    :param multiplier: Value used to calculate armour, default is 2. *Optional*
+    :type multiplier: int
     :example:
-    >>> from health_armour import Health
-    >>> zombie = Health(level=20, health_cap=55, outbreak=False, multiplier=2)
-    >>> zombie_health = zombie.get_health # 2450
-    >>> zombie_armour = zombie.get_armour # 1225
-    :note: The multiplier is what number you would like to divide the health number by to get armour health. Prior to season 4
-        this was half the zombies health.
+        >>> from health_armour import Health
+        >>> zombie = Health(level=20, health_cap=55, outbreak=False, multiplier=2)
+        >>> zombie_health = zombie.get_health # 2,450
+        >>> zombie_armour = zombie.get_armour # 1,225
+    :note: The multiplier is what number you would like to divide the health number by to get armour health.
+        Prior to season 4 this was half the zombies health.
     """
-    def __init__(self, level: int, health_cap: int = 55, outbreak: bool = False, multiplier: int = 2):
-
-        self.level = level
-        self.health_cap = health_cap
-        self.outbreak = outbreak
-        self.multiplier = multiplier
+    def __init__(self, level: int, health_cap: Optional[int] = 55, outbreak: Optional[bool] = False,
+                 multiplier: Optional[int] = 2):
+        self._level = level
+        self._health_cap = health_cap
+        self._outbreak = outbreak
+        self._multiplier = multiplier
 
         health_lst = []
         health_amount = 150
-        for num in range(max(self.health_cap, self.level) + 1):
+        for num in range(max(self._health_cap, self._level) + 1):
             if 1 < num <= 11:
                 health_amount += 60
             elif 11 < num <= 16:
@@ -54,12 +58,11 @@ class Health:
                 health_amount += 2000
             elif health_cap < num:
                 pass
-                # health_amount = 120000
             health_lst.append(health_amount)
 
         if outbreak:
             out_health_lst = []
-            for num in range(self.level):
+            for num in range(self._level):
                 if num <= 1:
                     out_health_lst.append(150)
                 elif 1 < num < 10:
@@ -67,10 +70,10 @@ class Health:
                 else:
                     out_health_lst.append(health_lst[-1])
 
-            self.hp = out_health_lst[self.level]
+            self._hp = out_health_lst[self._level]
         else:
-            self.hp = health_lst[self.level]
-        self.armour = float(int(self.hp / self.multiplier))
+            self._hp = health_lst[self._level]
+        self._armour = float(int(self._hp / self._multiplier))
 
     def __repr__(self):
         return "Zombie Health"
@@ -78,23 +81,16 @@ class Health:
     @property
     def get_health(self) -> float:
         """Returns zombie health value."""
-        return float(self.hp)
+        return float(self._hp)
 
     @property
     def get_armour(self) -> float:
         """Returns zombie armour value. Multiplier is an int you want to divide the zombie health level by
-            If 2, the armour health will be half the zombie health value.
-
-        **Parameters**
-
-        multiplier : int, default is 2
-            Divisor to get armour health value
-
-        """
-        return float(self.armour)
+            If 2, the armour health will be half the zombie health value."""
+        return float(self._armour)
 
     @get_armour.setter
     def get_armour(self, val):
         """Setter for updating the multiplier and armour value."""
-        self.multiplier = val
-        self.armour = float(int(self.hp / self.multiplier))
+        self._multiplier = val
+        self._armour = float(int(self._hp / self._multiplier))
