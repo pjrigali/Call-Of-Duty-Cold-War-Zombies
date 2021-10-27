1e16fadc7de43c84fa48337eb78f6995c173d8d8
Peter <prigali>
2021-10-27 00:05:07 -0500

    Update streamlit file

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ from zombie.processor import DamageProfile
 from zombie.analysis import Analyze
 import streamlit as st
 
+st.header('Call-Of-Duty-Cold-War-Zombies')
 st.multiselect('Multiselect', ['yes', 'no', 'maybe'])
