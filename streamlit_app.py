03c4fc29ee430630497221124ef5f449ecd77b54
Peter <prigali>
2021-10-27 02:01:59 -0500

    Update streamlit file

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import streamlit as st
 st.title('Call-Of-Duty-Cold-War-Zombies')
 st.markdown('*This is a demo of the Cold-War-Zombies package.*')
 st.subheader('Select Zombie Level:')
-zom_level = st.slider('Round Value', 1, 100, 1)
+zom_level = st.slider('Round Value', 0, 100, 1)
 
 st.sidebar.title("How to Use:")
 st.sidebar.markdown("1. Select the desired zombie round")
@@ -18,40 +18,40 @@ st.sidebar.markdown("4. View comparison tables below")
 st.sidebar.title("Weapons")
 st.sidebar.markdown("*All weapons are included up until mid-season 6.*")
 
+if zom_level > 0:
+    gun_lst = []
+    st.subheader('Select Weapons for Analysis')
+    weapon_lst = st.multiselect('Weapons', list(_weapon_stats_dic.keys()))
+    for weapon in weapon_lst:
+        st.subheader(weapon + ' Attachments:')
+        weapon_muzzle = st.selectbox(weapon + ' Weapon Muzzle', ['None'] + list(_weapon_stats_dic[weapon].muzzle.keys()))
+        weapon_barrel = st.selectbox(weapon + ' Weapon Barrel', ['None'] + list(_weapon_stats_dic[weapon].barrel.keys()))
+        weapon_body = st.selectbox(weapon + ' Weapon Body', ['None'] + list(_weapon_stats_dic[weapon].body.keys()))
+        weapon_ubarrel = st.selectbox(weapon + ' Weapon Underbarrel', ['None'] + list(_weapon_stats_dic[weapon].under_barrel.keys()))
+        weapon_mag = st.selectbox(weapon + ' Weapon Magazine', ['None'] + list(_weapon_stats_dic[weapon].magazine.keys()))
+        weapon_handle = st.selectbox(weapon + ' Weapon Handle', ['None'] + list(_weapon_stats_dic[weapon].handle.keys()))
+        weapon_stock = st.selectbox(weapon + ' Weapon Stock', ['None'] + list(_weapon_stats_dic[weapon].stock.keys()))
+        rarity = st.selectbox(weapon + ' Weapon Rarity', ['common', 'green', 'blue', 'purple', 'orange'])
+        pap = st.selectbox(weapon + ' Weapon Pack-a-punch level', ['0', '1', '2', '3'])
+        attachments = {
+            'Muzzle': weapon_muzzle,
+            'Barrel': weapon_barrel,
+            'Body': weapon_body,
+            'Underbarrel': weapon_ubarrel,
+            'Magazine': weapon_mag,
+            'Handle': weapon_handle,
+            'Stock': weapon_stock
+        }
+        val = {'weapon': weapon, 'nickname': 'Temp ' + weapon, 'equipped_attachments': attachments,
+               'rarity': rarity, 'pap': pap, 'accuracy': None, 'critical': None}
+        gun_lst.append(val)
 
-gun_lst = []
-st.subheader('Select Weapons for Analysis')
-weapon_lst = st.multiselect('Weapons', list(_weapon_stats_dic.keys()))
-for weapon in weapon_lst:
-    st.subheader(weapon + ' Attachments:')
-    weapon_muzzle = st.selectbox(weapon + ' Weapon Muzzle', ['None'] + list(_weapon_stats_dic[weapon].muzzle.keys()))
-    weapon_barrel = st.selectbox(weapon + ' Weapon Barrel', ['None'] + list(_weapon_stats_dic[weapon].barrel.keys()))
-    weapon_body = st.selectbox(weapon + ' Weapon Body', ['None'] + list(_weapon_stats_dic[weapon].body.keys()))
-    weapon_ubarrel = st.selectbox(weapon + ' Weapon Underbarrel', ['None'] + list(_weapon_stats_dic[weapon].under_barrel.keys()))
-    weapon_mag = st.selectbox(weapon + ' Weapon Magazine', ['None'] + list(_weapon_stats_dic[weapon].magazine.keys()))
-    weapon_handle = st.selectbox(weapon + ' Weapon Handle', ['None'] + list(_weapon_stats_dic[weapon].handle.keys()))
-    weapon_stock = st.selectbox(weapon + ' Weapon Stock', ['None'] + list(_weapon_stats_dic[weapon].stock.keys()))
-    rarity = st.selectbox(weapon + ' Weapon Rarity', ['common', 'green', 'blue', 'purple', 'orange'])
-    pap = st.selectbox(weapon + ' Weapon Pack-a-punch level', ['0', '1', '2', '3'])
-    attachments = {
-        'Muzzle': weapon_muzzle,
-        'Barrel': weapon_barrel,
-        'Body': weapon_body,
-        'Underbarrel': weapon_ubarrel,
-        'Magazine': weapon_mag,
-        'Handle': weapon_handle,
-        'Stock': weapon_stock
-    }
-    val = {'weapon': weapon, 'nickname': 'Temp ' + weapon, 'equipped_attachments': attachments,
-           'rarity': rarity, 'pap': pap, 'accuracy': None, 'critical': None}
-    gun_lst.append(val)
-
-weapon_class_levels = {'Launcher': '5', 'Special': '5', 'Smg': '5', 'Shotgun': '5', 'Pistol': '5',
-                       'Marksman': '5', 'Sniper': '5', 'Lmg': '5', 'Assault': '5', 'Melee': '5'}
-perk_class_levels = {'speed': '5', 'stamin up': '5', 'deadshot': '5', 'death_perception': '5'}
-damage_profile = DamageProfile(weapon_class_levels=weapon_class_levels, perk_class_levels=perk_class_levels,
-                               max_range=100)
-zom = Health(level=zom_level, health_cap=55, outbreak=False, multiplier=2)
+    weapon_class_levels = {'Launcher': '5', 'Special': '5', 'Smg': '5', 'Shotgun': '5', 'Pistol': '5',
+                           'Marksman': '5', 'Sniper': '5', 'Lmg': '5', 'Assault': '5', 'Melee': '5'}
+    perk_class_levels = {'speed': '5', 'stamin up': '5', 'deadshot': '5', 'death_perception': '5'}
+    damage_profile = DamageProfile(weapon_class_levels=weapon_class_levels, perk_class_levels=perk_class_levels,
+                                   max_range=100)
+    zom = Health(level=zom_level, health_cap=55, outbreak=False, multiplier=2)
 
 # Build Analyze Class
 if len(gun_lst) >= 1:
