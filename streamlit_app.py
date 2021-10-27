f3d63858ba3339eb4d4f815ba10cfdf8396800a3
Peter <prigali>
2021-10-27 00:24:08 -0500

    Update streamlit file

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,9 @@ import streamlit as st
 
 st.header('Call-Of-Duty-Cold-War-Zombies')
 first_weapon = st.multiselect('First Weapon', list(_weapon_stats_dic.keys()))
-t = list(_weapon_stats_dic['MP5'].muzzle.keys())
-first_weapon_muzzle = st.multiselect('First Weapon Muzzle', t)
+fwm = list(_weapon_stats_dic[first_weapon].muzzle.keys())
+
+first_weapon_muzzle = st.multiselect('First Weapon Muzzle', fwm)
 
 # M16 = {
 #     'Muzzle': 'Agency Suppressor',
