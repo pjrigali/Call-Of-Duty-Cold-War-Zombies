4950b74f8606b3f4f2c24ac6197ad6e0e54d8215
Peter <prigali>
2021-10-27 00:03:13 -0500

    Add streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+from zombie.health_armour import Health
+from zombie.processor import DamageProfile
+from zombie.analysis import Analyze
+import streamlit as st
+
+st.multiselect('Multiselect', ['yes', 'no', 'maybe'])
